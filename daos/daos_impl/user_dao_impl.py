b83922352405443d853f10717800e3c2b9b27d5c
VanVelZ <zachofasunset@me.com>
2021-05-12 10:53:40 -0500

    added staticmethod to userimpl

--- daos/daos_impl/user_dao_impl.py
+++ daos/daos_impl/user_dao_impl.py
@@ -4,7 +4,8 @@ from daos.user_dao import UserDAO
 
 class UserDAOImpl(UserDAO):
 
-    def create_user(self, user):  # Create new User
+    @staticmethod
+    def create_user(user):  # Create new User
         sql = "INSERT INTO users VALUES(DEFAULT, %s, %s, %s ) RETURNING *"
         cursor = connection.cursor()
         cursor.execute(sql, [user.first_name,  user.last_name, user.login_id, user.password])
@@ -14,7 +15,8 @@ class UserDAOImpl(UserDAO):
         return Employee(
             User(record[0], record[1], record[2], record[3], record[4]).json())
 
-    def get_all_users(self):  # Retrieve all User from User and return
+    @staticmethod
+    def get_all_users():  # Retrieve all User from User and return
         sql = "SELECT * FROM users"
         cursor = connection.cursor()
         cursor.execute(sql)
