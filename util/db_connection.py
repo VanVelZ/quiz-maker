6f969c77e50a127e3590db443c6615f76bfe4bdc
Christopher Sei <chrissei@ChristophersMBP.home>
2021-05-14 16:10:32 -0400

    updating course

--- util/db_connection.py
+++ util/db_connection.py
@@ -6,10 +6,10 @@ def create_connection():
     try:
         conn = psycopg2.connect(
             database='postgres',
-            user='chris',
-            password='Spring4164',
-            host='dbinstance1postgres.clfg8r6mmtbs.us-east-2.rds.amazonaws.com',
-            port='5432'
+            user='',
+            password='',
+            host='',
+            port=''
 
         )
         return conn
