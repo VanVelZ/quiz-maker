463d92489971c3a38289753608d3ae71a8e9e451
alexjones <akjones1992@yahoo.com>
2021-05-17 15:32:51 -0500

    redirect from login added

--- presentation/scripts/student_quizzes.js
+++ presentation/scripts/student_quizzes.js
@@ -1,3 +1,33 @@
+console.log(user)
+
+function getStudentInfo() {
+    event.preventDefault();
+
+    thisUserId = user.userId
+
+        let xhttp = new XMLHttpRequest();
+         xhttp.onreadystatechange = function () {
+
+        if (this.readyState == 4 && this.status == 200) {
+            let user = JSON.parse(this.responseText)
+            console.log(user)
+                
+        document.getElementById("studentName").innerHTML = ""
+        document.getElementById("studentId").innerHTML = ""
+        document.getElementById("classes").innerHTML = ""
+        }
+
+    }
+
+
+
+    url = url = "http://127.0.0.1:5000/users/" + thisUserId
+
+    xhttp.open("GET", url, true)
+    xhttp.send()
+}
+getStudentInfo()
+
 function logout() {
     event.preventDefault();
 
