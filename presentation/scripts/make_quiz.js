521d319f79bfe1bba26147b2b8afb7fa240812d7
Zach <50152024+VanVelZ@users.noreply.github.com>
2021-05-17 22:09:28 -0500

    Finished setting up quiz maker for quick javascript

--- presentation/scripts/make_quiz.js
+++ presentation/scripts/make_quiz.js
@@ -1,3 +1,4 @@
+questionCount = 1
 function logout() {
     event.preventDefault();
 
@@ -18,6 +19,32 @@ function logout() {
 
     xhttp.open("DELETE", url, true)
     xhttp.send()
+}
 
+function addQuestion(){
+    questionCount += 1
+    document.getElementById("quiz").innerHTML += 
+    `
+    <tr>
+        <td class="dataName">Question ${questionCount}</td>
+        <td id="q1"><input class="q${questionCount}Description" name="q${questionCount}" type="text" placeholder="Question"></td>
+    </tr>
+    <tr>
+        <td id="a1_1"><input class="q${questionCount}Answer" name="a${questionCount}_1" type="text" placeholder="Answer"></td>
+        <td id="option1_1Radio"> A <input type="radio" name="q${questionCount}Radio" value="a${questionCount}_1"> Correct Answer </td>
+    </tr>
+    <tr>
+        <td id="a1_2"><input class="q${questionCount}Answer" name="a${questionCount}_2" type="text" placeholder="Answer"></td>
+        <td id="option1_2Radio"> B <input type="radio" name="q${questionCount}Radio" value="a${questionCount}_2"> Correct Answer </td>
+    </tr>
+    <tr>
+        <td id="a1_3"><input class="q${questionCount}Answer" name="a${questionCount}_3" type="text" placeholder="Answer"></td>
+        <td id="option1_3Radio"> C <input type="radio" name="q${questionCount}Radio" value="a${questionCount}_3"> Correct Answer </td>
+    </tr>
+    <tr>
+        <td id="a1_4"><input class="q${questionCount}Answer" name="a${questionCount}_4" type="text" placeholder="Answer"></td>
+        <td id="option1_4Radio"> D <input type="radio" name="q${questionCount}Radio" value="a${questionCount}_4"> Correct Answer </td>
+    </tr>
+    `
 
 }
