e3c7ed6addc770eb5321b38a70e99d8572993677
Zach <50152024+VanVelZ@users.noreply.github.com>
2021-05-17 22:39:18 -0500

    properly pulling in list of question names

--- presentation/scripts/make_quiz.js
+++ presentation/scripts/make_quiz.js
@@ -1,15 +1,29 @@
+
+//Globals
 questionCount = 1
+class Question {
+    constructor(name) {
+        this.name = name
+        this.answers = []
+    }
+}
+class Answer {
+    constructor(name, isCorrect = false) {
+        this.name = name
+        this.isCorrect = isCorrect
+    }
+}
+
 function logout() {
-    event.preventDefault();
 
         let xhttp = new XMLHttpRequest();
 
 
-            xhttp.onreadystatechange = function () {
+        xhttp.onreadystatechange = function () {
                 
-        document.getElementById("name").innerHTML = ""
-        document.getElementById("employee_id").innerHTML = ""
-        document.getElementById("supervisor_id").innerHTML = ""
+            document.getElementById("name").innerHTML = ""
+            document.getElementById("employee_id").innerHTML = ""
+            document.getElementById("supervisor_id").innerHTML = ""
 
     }
 
@@ -20,14 +34,23 @@ function logout() {
     xhttp.open("DELETE", url, true)
     xhttp.send()
 }
-
+function makeQuiz() {
+    quizName = document.getElementById("quizName").value
+    questionDescriptions = document.getElementsByClassName("questionDescription")
+    questions = []
+    Array.prototype.forEach.call(questionDescriptions, function (element) {
+        questions.push(new Question(element.value))
+    });
+    console.log(quizName)
+    console.log(questions)
+}
 function addQuestion(){
     questionCount += 1
     document.getElementById("quiz").innerHTML += 
     `
     <tr>
         <td class="dataName">Question ${questionCount}</td>
-        <td id="q1"><input class="q${questionCount}Description" name="q${questionCount}" type="text" placeholder="Question"></td>
+        <td id="q1"><input class="questionDescription" name="q${questionCount}" type="text" placeholder="Question" id="q${questionCount}Description"></td>
     </tr>
     <tr>
         <td id="a1_1"><input class="q${questionCount}Answer" name="a${questionCount}_1" type="text" placeholder="Answer"></td>
@@ -46,5 +69,4 @@ function addQuestion(){
         <td id="option1_4Radio"> D <input type="radio" name="q${questionCount}Radio" value="a${questionCount}_4"> Correct Answer </td>
     </tr>
     `
-
 }
