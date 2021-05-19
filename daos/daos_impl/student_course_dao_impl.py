ae9095ffd5031c00e1d8012eaac8e25447a0252c
Zach <50152024+VanVelZ@users.noreply.github.com>
2021-05-18 22:07:29 -0500

    Fixed conflicts

--- daos/daos_impl/student_course_dao_impl.py
+++ daos/daos_impl/student_course_dao_impl.py
@@ -6,10 +6,6 @@ from util.db_connection import connection
 
 class StudentCourseDaoImpl(StudentCoursesDAO):
 
-<<<<<<< HEAD
-=======
-
->>>>>>> parent of 9c6cc3c (Merge pull request #32 from VanVelZ/DonBranch)
     @staticmethod
     def get_all_student_courses():  # retrieve all student courses
         sql = "SELECT * FROM student_courses"
@@ -22,12 +18,8 @@ class StudentCourseDaoImpl(StudentCoursesDAO):
             studentcourses_list.append(student_courses.json())
         return studentcourses_list
 
-<<<<<<< HEAD
     @staticmethod
     def get_courses_by_id(course_id):
-=======
-    def get_courses_by_id(self,  courseid):
->>>>>>> parent of 9c6cc3c (Merge pull request #32 from VanVelZ/DonBranch)
         sql = "SELECT * FROM student_courses where id=%s"
         cursor = connection.cursor()
         cursor.execute(sql, [course_id])
@@ -36,10 +28,5 @@ class StudentCourseDaoImpl(StudentCoursesDAO):
         if record:
             return StudentCourses(record[0], record[1], record[2])
         else:
-<<<<<<< HEAD
             raise ResourceNotFound(
                 f"Student course id: {course_id} - Not Found")
-=======
-            raise ResourceNotFound(f"Student course id: {courseid} - Not Found")
-
->>>>>>> parent of 9c6cc3c (Merge pull request #32 from VanVelZ/DonBranch)
