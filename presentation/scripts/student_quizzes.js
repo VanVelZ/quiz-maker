028c71f8d6ef40e3434429f8100d250422727af8
alexjones <akjones1992@yahoo.com>
2021-05-19 11:51:06 -0500

    updaded logouts and students current courses now appear in info

--- presentation/scripts/student_quizzes.js
+++ presentation/scripts/student_quizzes.js
@@ -38,15 +38,27 @@ function getStudentClasses() {
 
         if (this.readyState == 4 && this.status == 200) {
             let classesRes = JSON.parse(this.responseText)
-            let classes
-            console.log(classesRes)
+            let classes = ''
+            console.log(classesRes[2].studentId)
             for (i = 0; i < classesRes.length; i++) {
                 if (classesRes[i].studentId == thisUserId) {
-                    clases += " " + classesRes[i]
+                    if (classesRes[i].courseId == 1){
+                        classes += "Physical Science, "
+                    } else if (classesRes[i].courseId == 2) {
+                        classes += "Biology, "
+                    }
+                    
                 }
             }
             console.log(classes)
-            document.getElementById("classes").innerHTML = ""
+            document.getElementById("classes").innerHTML = classes
+            // console.log(courses)
+            // Array.prototype.forEach.call(courses, function (course) {
+            //     document.getElementById("classes").innerHTML = `
+            //         ${course.coursesId}${course.name}
+            //     `
+            // });
+
         }
 
     }
@@ -58,6 +70,29 @@ function getStudentClasses() {
     xhttp.open("GET", url, true)
     xhttp.send()
 
+}
+
+function getCourses(){
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let courses = JSON.parse(this.response)
+            console.log(courses)
+            Array.prototype.forEach.call(courses, function (course) {
+                document.getElementById("courseSelect").innerHTML = `
+                    <option value=${course.coursesId}>${course.name}</option>
+                `
+            });
+        } 
+    }
+
+    let url = "http://127.0.0.1:5000/courses/studentcourses"
+
+    xhr.open("GET", url, true)
+
+    xhr.send()
 }
 
 function logout(event) {
