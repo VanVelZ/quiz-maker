908230331999d006a10f6eb603dbb5e135b30199
Zach <50152024+VanVelZ@users.noreply.github.com>
2021-05-18 22:01:57 -0500

    Update answers_dao.py

--- daos/answers_dao.py
+++ daos/answers_dao.py
@@ -2,12 +2,8 @@ from abc import abstractmethod, ABC
 
 
 class AnswersDAO(ABC):
-<<<<<<< HEAD
     @staticmethod
     @abstractmethod
-=======
-
->>>>>>> parent of 9c6cc3c (Merge pull request #32 from VanVelZ/DonBranch)
     def get_all_answers_for_question(question_id):
         pass
 
