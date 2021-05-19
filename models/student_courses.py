7e6d2aa19d734f439baa543fc701f3036e51273e
Zach <50152024+VanVelZ@users.noreply.github.com>
2021-05-19 15:22:47 -0500

    Basic displaying of courses in student quiz

--- models/student_courses.py
+++ models/student_courses.py
@@ -1,22 +1,18 @@
 class StudentCourses:
 
-    def __init__(self, student_courses=0, student_id=0, course_id=0):
-        self.student_courses = student_courses
+    def __init__(self, student_id=0, course_id=0):
         self.student_id = student_id
         self.course_id = course_id
 
     def json(self):
         return {
-            'studentCourses': self.student_courses,
             'studentId': self.student_id,
             'courseId': self.course_id,
-
         }
 
     @staticmethod
     def json_parse(json):
         student_courses = StudentCourses()
-        student_courses.student_courses = json["studentCourses"]
         student_courses.student_id = json["studentId"]
         student_courses.course_id = json["courseId"]
 
